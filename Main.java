84f8d5bf8d5567da95a490e0fb0a02e96e1f7b97
Priyanka Shah <shah.priyanka9@gmail.com>
2014-12-12 22:21:47 -0500

    add comments

--- Main.java
+++ Main.java
@@ -2,13 +2,23 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		sayHello();
+        // These are method calls
+		sayHello(); 
+		saySomething("Yey");
 
 	}
 
+	// This is a method that prints hello world to STDOUT
 	public static void sayHello() {
 
 		System.out.println("Hello, World!");
+
+	}
+
+	// This is a method that prints a message to STDOUT
+	public static void saySomething(String msg) {
+
+		System.out.println(msg);
 
 	}
 
